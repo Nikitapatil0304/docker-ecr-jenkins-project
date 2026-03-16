be88bc72d1f4ff8799f2449fc381a81e10f92e23
Nikitapatil0304 <patilmau014@gmail.com>
2026-03-16 14:44:59 +0530

    Integrate DynamoDB logging into lambda function
    Added DynamoDB logging for new Docker images pushed.

--- lambda/lambda-function.py
+++ lambda/lambda-function.py
@@ -3,8 +3,11 @@ import boto3
 from datetime import datetime
 
 sns = boto3.client('sns')
+dynamodb = boto3.resource('dynamodb')
 
-topic_arn = "SNS_TOPIC_ARN"
+topic_arn = "arn:aws:sns:ap-south-1:718383533665:docker-deployment-alert"
+
+table = dynamodb.Table('ecr-image-logs')
 
 def lambda_handler(event, context):
 
@@ -13,10 +16,20 @@ def lambda_handler(event, context):
 
     message = f"New image pushed {repo}:{tag}"
 
+    # SNS Notification
     sns.publish(
         TopicArn=topic_arn,
         Message=message,
         Subject="Docker Deployment"
+    )
+
+    # DynamoDB Logging
+    table.put_item(
+        Item={
+            'imageTag': tag,
+            'repository': repo,
+            'timestamp': str(datetime.now())
+        }
     )
 
     return {
